644927ff9b97677c851ab5b5cac47bf46cb09243
praveen05git <48729839+praveen05git@users.noreply.github.com>
2019-07-31 14:05:15 +0530

    Create Reverse a number JAVA.java

---
+++ Reverse a number/Reverse a number JAVA.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+        int numb=123,rev,temp;
+        
+        while(numb>0)
+        {
+            temp=numb%10;
+            rev=rev*10+temp;
+            numb=numb/10;
+        }
+        
+        System.out.println(numb);
+     }
+}
