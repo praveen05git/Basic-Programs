f0aab8189d8f9cac0e6d8b880594d8fae9c4140a
praveen05git <48729839+praveen05git@users.noreply.github.com>
2019-08-02 12:11:15 +0530

    Create Concat using join.py

---
+++ Concatenate two strings/Concat using join.py
@@ -0,0 +1,8 @@
+s1 = 'abc'
+s2 = '123'
+
+""" Each character of s2 is concatenated to the front of s1""" 
+print('s1.join(s2):', s1.join(s2))
+
+""" Each character of s1 is concatenated to the front of s2""" 
+print('s2.join(s1):', s2.join(s1))
