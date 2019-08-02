0679a94fb86ea30b9a75d2f117361b90f78f0638
praveen05git <48729839+praveen05git@users.noreply.github.com>
2019-08-02 11:31:54 +0530

    Create Factorial.java

---
+++ Factorial/Factorial.java
@@ -0,0 +1,18 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 10;
+        long factorial = 1;
+        for(int i = 1; i <= num; ++i)
+        {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        System.out.printf("Factorial of %d = %d", num, factorial);
+    }
+}
+
+
+/*
+LOGIC:
+5!=1*2*3*4*5
+*/
