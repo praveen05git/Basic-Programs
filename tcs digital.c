c4443cffc1a4ffd43d09af978348dcb418d22919
praveen05git <48729839+praveen05git@users.noreply.github.com>
2019-08-21 10:58:36 +0530

    Create tcs digital.c

---
+++ tcs digital.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int a[n][n];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+            scanf("%d",&a[i][j]);
+    } 
+    int min=a[0][0];
+    i=0,j=0;
+    while(i<n && j<n)
+    {
+        if(i==n-1 && j==n-1)
+        break;
+        if(j!=(n-1) && i!=(n-1) && a[i][j+1]<a[i+1][j])
+        {
+            if(a[i][j]>a[i][j+1])
+            {
+                min=a[i][j];
+                printf("%d\n",min);
+                j++;
+            }
+        else
+            {
+min=a[i][j+1];
+printf("%d\n",min);
+j++;
+}
+}
+else if( j!=(n-1) && i!=(n-1) && a[i+1][j]<a[i][j+1])
+{
+if(a[i][j]>a[i+1][j])
+{
+min=a[i][j];
+printf("%d\n",min);
+i++;
+}
+else
+{
+min=a[i+1][j];
+printf("%d\n",min);
+i++;
+}
+}
+if(j==n-1)
+{
+if(a[i+1][j]>min)
+{
+min=a[i+1][j];
+printf("%d\n",min);
+i++;
+}
+else
+{
+// min=a[i][j];
+printf("%d\n",min);
+i++;
+}
+}
+else 
+{
+if(i==n-1)
+{
+if(a[i][j+1]>min)
+{
+min=a[i][j+1];
+printf("%d\n",min);
+j++;
+}
+else
+{
+// min=a[i][j];
+printf("%d\n",min);
+j++;
+}
+}
+}
+}
+printf("%d",min);
+return 0;
+}
